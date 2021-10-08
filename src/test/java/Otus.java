4204e7d84a30f5dd086ab89855a4a41343d3259b
iluha5596 <pantik96@mail.ru>
2021-10-08 10:48:09 +0300

    Version 1.0

--- src/test/java/Otus.java
+++ src/test/java/Otus.java
@@ -19,7 +19,7 @@ public class Otus {
     public void StartUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        logger.info("Драйвер поднят");
+        logger.info("Driver up");
         driver.manage().window().maximize();
         driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 
@@ -34,14 +34,14 @@ public class Otus {
 
     private void auth() throws InterruptedException  {
         driver.get("https://otus.ru");
-        logger.info("Сайт открыт");
+        logger.info("Site open");
         try {
             Thread.sleep(20);
             driver.findElement(By.xpath("//button[@class=\"header2__auth js-open-modal\"]")).click();
             driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/div[2]/form/div[2]/input")).sendKeys("pantik96@mail.ru");
             driver.findElement(By.xpath("//input[@type=\"password\"]")).sendKeys("qwerty123");
             driver.findElement(By.xpath("/html/body/div[2]/div/div/div/div[3]/div[2]/div[2]/form/div[4]/button")).click();
-            logger.info("Авторизация прошла успешно");
+            logger.info("Authorization path");
 
         } catch (InterruptedException e) {
             e.printStackTrace();
@@ -50,14 +50,44 @@ public class Otus {
 
     private void my() throws InterruptedException  {
         driver.get("https://otus.ru/lk/biography/personal/");
-        logger.info("Раздел о себе открыт");
+        logger.info("Paragraph My open");
+
+    }
+
+    private void addMy() throws InterruptedException {
+        //Очистка данных
+        driver.findElement(By.xpath("//input[@data-title=\"Имя\"]")).clear();
+        driver.findElement(By.xpath("//input[@data-title=\"Фамилия\"]")).clear();
+        driver.findElement(By.xpath("//input[@name=\"blog_name\"]")).clear();
+        driver.findElement(By.xpath("//input[@title=\"День рождения\"]")).clear();
+        //Заполнение данных
+        driver.findElement(By.xpath("//input[@data-title=\"Имя\"]")).sendKeys("Илья");
+        driver.findElement(By.xpath("//input[@data-title=\"Фамилия\"]")).sendKeys("Пантиков");
+        driver.findElement(By.xpath("//input[@name=\"blog_name\"]")).sendKeys("Ilya");
+        driver.findElement(By.xpath("//input[@title=\"День рождения\"]")).sendKeys("05.07.1996");
+        driver.findElement(By.xpath("//button[@title=\"Россия\"]")).click();
+        driver.findElement(By.xpath("//button[@title=\"Москва\"]")).click();
+        driver.findElement(By.xpath("//button[@title=\"Элементарный уровень (Elementary)\"]")).click();
+        driver.findElement(By.xpath("//input[@title=\"Полный день\"]//..")).click();
+        driver.findElement(By.xpath("//button[@data-value=\"whatsapp\"]")).click();
+        driver.findElement(By.xpath("//input[@name=\"contact-0-value\"]")).sendKeys("+79999999999");
+        driver.findElement(By.xpath("//button[text() = \"Добавить\"]")).click();
+        driver.findElement(By.xpath("(//button[@data-value=\"telegram\"])[2]")).click();
+        driver.findElement(By.xpath("(//button[@data-value=\"telegram\"])[2]")).sendKeys("+79999999999");
 
     }
 
     @Test
     public void PersonalArea() throws InterruptedException  {
+        //Авторизация
         auth();
+        //Переход в раздел о себе
+        Thread.sleep(20);
         my();
+        //Заполнение данных
+        Thread.sleep(10);
+        addMy();
+
     }
 
     private class Authorization extends Exception {
